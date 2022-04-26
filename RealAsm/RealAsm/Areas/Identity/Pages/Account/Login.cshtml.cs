323503afc9836dab12da49969db65287b8bd098e
trungadqe <86902641+trungadqe@users.noreply.github.com>
2022-04-26 16:38:03 +0700

    login with cusomer or seller
    login return customer other seller

--- RealAsm/RealAsm/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ RealAsm/RealAsm/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -2,19 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using RealAsm.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace RealAsm.Areas.Identity.Pages.Account
 {
@@ -22,11 +15,13 @@ namespace RealAsm.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<RealAsmUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly UserManager<RealAsmUser> _userManager;
 
-        public LoginModel(SignInManager<RealAsmUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<RealAsmUser> signInManager, ILogger<LoginModel> logger, UserManager<RealAsmUser> userManager)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _userManager = userManager;
         }
 
         /// <summary>
@@ -115,7 +110,17 @@ namespace RealAsm.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    /*LocalRedirect(returnUrl);*/
+                    RealAsmUser userName = await _userManager.FindByNameAsync(Input.UserName);
+                    var rolesname = await _userManager.GetRolesAsync(userName);
+                    if (rolesname.Contains("Customer"))
+                    {
+                        return RedirectToAction("HomeCustomerIndex", "Home", new { area = "" });
+                    }
+                    if (rolesname.Contains("Seller"))
+                    {
+                        return RedirectToPage("HomeSellerIndex.cshtml", "Home", new { area = "" });
+                    }
                 }
                 if (result.RequiresTwoFactor)
                 {
--- RealAsm/RealAsm/Controllers/HomeController.cs
+++ RealAsm/RealAsm/Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RealAsm.Areas.Identity.Data;
 using RealAsm.Models;
 using System.Diagnostics;
+using System.Linq;
 
 namespace RealAsm.Controllers
 {
@@ -13,17 +15,49 @@ namespace RealAsm.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IEmailSender _emailSender;
         private readonly UserManager<RealAsmUser> _userManager;
-        public HomeController(ILogger<HomeController> logger, IEmailSender emailSender, UserManager<RealAsmUser> userManager)
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+
+        public HomeController(ILogger<HomeController> logger, IEmailSender emailSender, UserManager<RealAsmUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _logger = logger;
             _emailSender = emailSender;
             _userManager = userManager;
+            _roleManager = roleManager;
         }
-        [Authorize(Roles = "Customer")]
-        public async Task<IActionResult> Test()
+        public List<IdentityRole> roles { set; get; }
+        public System.Security.Claims.ClaimsPrincipal GetUser()
         {
-            ViewBag.message = "This is for Customer only! Hi " + _userManager.GetUserId(HttpContext.User);
-            return View("Views/Home/Index.cshtml");
+            return User;
+        }
+
+        public IActionResult HomeCustomerIndex()
+        {
+            return View();
+        }
+        public IActionResult HomeSellerIndex()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Testrolelogin()
+        {
+            /*var roles = await _roleManager.GetRoleNameAsync(role);
+            ViewBag.message = "this role is:" + roles;*/
+            var userName = await _userManager.GetUserAsync(HttpContext.User);
+            /*ViewBag.message = "this user name is:" + userName;*/
+            /*var userID = await _userManager.GetUserIdAsync(userName);
+            ViewBag.message = userID;*/
+            var rolesname = await _userManager.GetRolesAsync(userName);
+            
+            if (rolesname.Contains("Customer"))
+            {
+                return View("Views/Home/Index.cshtml");
+            }
+            /*ViewBag.message = roles;*/ // đang chỉnh sữa
+            /*var userRoles = await _userManager.GetRolesAsync(userName);
+            ViewBag.message = userRoles;*/
+            /*ViewBag.message = "This is for Customer only! Hi " + _userManager.GetUserId(HttpContext.User);*/
+            return View();
         }
 
         [Authorize(Roles = "Customer")]
